57ad4bb372224f850325d3a331f3c4b683ab32ca
HSaleem352 <60048058+HSaleem352@users.noreply.github.com>
2024-01-15 00:18:52 -0500

    Update EmployeesSQL_Queries.sql

--- EmployeeSQL/EmployeesSQL_Queries.sql
+++ EmployeeSQL/EmployeesSQL_Queries.sql
@@ -1,13 +1,3 @@
--- display tables
-SELECT * FROM departments;
-SELECT * FROM dept_emp;
-SELECT * FROM dept_manager;
-SELECT * FROM employees;
-SELECT * FROM salaries;
-SELECT * FROM titles;
-
--- Data Analysis
-
 -- 1. List the employee number, last name, first name, sex, and salary of each employee.
 
 SELECT employees.emp_no
