df97dfc6b6a2ca0b6002a3aedcea4ebd42b47d62
HSaleem352 <60048058+HSaleem352@users.noreply.github.com>
2024-01-15 00:20:07 -0500

    Update QuickDBD-tables.sql

--- EmployeeSQL/QuickDBD-tables.sql
+++ EmployeeSQL/QuickDBD-tables.sql
@@ -1,11 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
-CREATE TABLE departments (
+﻿CREATE TABLE departments (
     "dept_no" varchar   NOT NULL,
     "dept_name" varchar   NOT NULL,
     CONSTRAINT "pk_Departments" PRIMARY KEY (
